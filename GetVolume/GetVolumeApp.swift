34e1356c82c9c734578101660a81d15b33f22903
kakizaki hayate <hayate.h.0704@gmail.com>
2023-11-21 16:07:37 +0900

    initial project

---
+++ GetVolume/GetVolumeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GetVolumeApp.swift
+//  GetVolume
+//
+//  Created by 柿崎逸 on 2023/11/21.
+//
+
+import SwiftUI
+
+@main
+struct GetVolumeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
